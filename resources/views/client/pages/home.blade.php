ecc4d4ffd03a6efcf505dd79881e273c8ed4de53
maiphuong0903 <maithiphuong09032002@gmail.com>
2023-09-29 18:08:14 +0700

    chi tiết sản phẩm

--- resources/views/client/pages/home.blade.php
+++ resources/views/client/pages/home.blade.php
@@ -6,7 +6,7 @@
 @section('content')
 <div>
     @include('client.components.home.banner')
-    <div class="mb-10 mt-20 2xl:mx-32 mx-5 lg:mx-8">
+    <div class="mb-10 pt-20 2xl:mx-32 mx-5 lg:mx-8">
         <h1 class="text-center font-bold text-2xl text-[#92d3f9]">FRAMEWORK PHỔ BIẾN NHẤT</h1>
         <hr class="mx-auto w-28 border-[#92d3f9] mt-2 mb-10"> 
         <div class="md:grid md:grid-cols-3 lg:gap-x-10 gap-x-6">
@@ -72,122 +72,35 @@
             </div>
         </div>            
     </div>
-    <div class="mb-10 mt-20 lg:mx-8 mx-4">
+    {{-- Bài viết --}}
+    <div class="pb-10 pt-20 lg:mx-8 mx-4">
         <h1 class="text-center font-bold text-2xl text-[#92d3f9]">BÀI VIẾT MỚI NHẤT</h1>
         <hr class="mx-auto w-28 border-[#92d3f9] mt-2 mb-10"> 
         <div class="grid grid-cols-2 md:grid-cols-4 gap-x-7 xl:mx-24">
-            <div class="mb-28 bg-white">
-                <img class="w-full h-full rounded-md" src="https://devsne.vn/image/post/15-useful-libraries-for-android-development-part-ii-18814004.jpg" alt="">
-                <div class="flex flex-col h-[90px]">
-                    <h1 class="pt-2">Title cả tiêu đề bài viết nhé ahhiiiiiiiiiiiiiiiiiiiiiiiiii</h1>
-                    <div class="flex 2xl:pr-1 mt-auto">
-                        <div>
-                            <h1>Tên tác giả</h1>
+            @foreach ($posts as $post)
+                <a  href="{{ route('client.post.detail', ['post' => $post->id]) }}" class="mb-10 bg-white rounded-lg overflow-hidden shadow-md">
+                    <img class="w-full h-[210px] object-cover" src="https://source.unsplash.com/random" alt="">
+                    <div class="flex flex-col h-[180px] px-2.5 pb-3">
+                        <div class="py-2 font-semibold text-gray-800 h-[60px] overflow-hidden">{{ $post->title }}</div>
+                        <div class="h-[60px] overflow-hidden mt-1">
+                            <p class="text-sm text-gray-500">
+                                {!! $post->content !!}
+                            </p>
                         </div>
-                        <div class="flex flex-1 justify-end">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
-                            </svg> 
-                        </div>                  
-                    </div>
-                </div>
-            </div>
-            <div class="mb-28">
-                <img class="w-full h-full rounded-md" src="https://devsne.vn/image/post/laravel-10-ajax-crud-operations-example-55597820.jpg" alt="">
-                <div class="flex flex-col h-[90px]">
-                    <h1 class="pt-2">Title cả tiêu đề bài viết nhé ahhiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii</h1>
-                    <div class="flex pr-1 mt-auto">
-                        <div>
-                            <h1>Tên tác giả</h1>
+                     
+                        <div class="flex 2xl:pr-1 mt-auto items-center">
+                            <div class="text-[12px] text-white px-3 py-0.5 rounded-full bg-[#292c45] capitalize">
+                                {{ $post->author->name}}
+                            </div>
+                            <div class="flex flex-1 justify-end">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 text-red-400">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
+                                </svg> 
+                            </div>                  
                         </div>
-                        <div class="flex flex-1 justify-end">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
-                            </svg> 
-                        </div>                  
                     </div>
-                </div>
-            </div>
-            <div class="mb-28">
-                <img class="w-full h-full rounded-md" src="https://devsne.vn/image/post/cach-sua-cac-goi-bi-hong-broken-packages-tren-ubuntu-16.04-va-debian-9-6165750.jpg" alt="">
-                <div class="flex flex-col h-[90px]">
-                    <h1 class="pt-2">Title cả tiêu đề bài viết nhé</h1>
-                    <div class="flex pr-1 mt-auto">
-                        <div>
-                            <h1>Tên tác giả</h1>
-                        </div>
-                        <div class="flex flex-1 justify-end">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
-                            </svg> 
-                        </div>                  
-                    </div>
-                </div>
-            </div>
-            <div class="mb-28">
-                <img class="w-full h-full rounded-md" src="https://devsne.vn/image/post/how-to-integrate-razorpay-payment-gateway-in-laravel-9-53713722.jpg" alt="">
-                <div class="flex flex-col h-[90px]">
-                    <h1 class="pt-2">Title cả tiêu đề bài viết nhé ahhiiiiiiiiiiiii</h1>
-                    <div class="flex pr-1 mt-auto">
-                        <div>
-                            <h1>Tên tác giả</h1>
-                        </div>
-                        <div class="flex flex-1 justify-end">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
-                            </svg> 
-                        </div>                  
-                    </div>
-                </div>
-            </div>
-            <div class="mb-28">
-                <img class="w-full h-full rounded-md" src="https://devsne.vn/image/post/giai-thich-ve-bo-tri-bo-nho-cua-chuong-trinh-c-c-.-9902847.jpg" alt="">
-                <div class="flex flex-col h-[90px]">
-                    <h1 class="pt-2">Title cả tiêu đề bài viết nhé ahhiiiiiiiiiiiii</h1>
-                    <div class="flex pr-1 mt-auto">
-                        <div>
-                            <h1>Tên tác giả</h1>
-                        </div>
-                        <div class="flex flex-1 justify-end">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
-                            </svg> 
-                        </div>                  
-                    </div>
-                </div>
-            </div>
-            <div class="mb-28">
-                <img class="w-full h-full rounded-md" src="https://devsne.vn/image/post/cai-thien-hieu-xuat-bang-debounce-trong-javascript-23367610.jpg" alt="">
-                <div class="flex flex-col h-[90px]">
-                    <h1 class="pt-2">Title cả tiêu đề bài viết nhé ahhiiiiiiiiiiiii</h1>
-                    <div class="flex pr-1 mt-auto">
-                        <div>
-                            <h1>Tên tác giả</h1>
-                        </div>
-                        <div class="flex flex-1 justify-end">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
-                            </svg> 
-                        </div>                  
-                    </div>
-                </div>
-            </div>
-            <div class="mb-28">
-                <img class="w-full h-full rounded-md" src="https://devsne.vn/image/post/gioi-thieu-dart-trong-flutter-co-ban-5665273.jpg" alt="">
-                <div class="flex flex-col h-[90px]">
-                    <h1 class="pt-2">Title cả tiêu đề bài viết nhé ahhiiiiiiiiiiiii</h1>
-                    <div class="flex pr-1 mt-auto">
-                        <div>
-                            <h1>Tên tác giả</h1>
-                        </div>
-                        <div class="flex flex-1 justify-end">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
-                            </svg> 
-                        </div>                  
-                    </div>
-                </div>
-            </div>
+                </a>
+            @endforeach
         </div>
     </div>
 </div>
